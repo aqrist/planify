51ffde75c5eb82493b86a1981e42aee7cfce337e
Aqris T. Satria <aqrist@gmail.com>
2025-05-08 13:18:47 +0700

    style: update font family and add typography plugin
    Update the font family from 'Figtree' to 'Roboto' and add the '@tailwindcss/typography' plugin to enhance text styling. Also, include 'react-icons' for additional icon support. These changes improve the overall design and consistency of the application.

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,6 +1,24 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
+
+export const STATUS = {
+    TODO: 'todo',
+    IN_PROGRESS: 'in_progress',
+    ONREVIEW: 'on_review',
+    DONE: 'done',
+    UNKNOWN: 'unknown',
+};
+
+export const PRIORITY = {
+    UNKNOWN: 'unknown',
+    LOW: 'low',
+    MEDIUM: 'medium',
+    HIGH: 'high',
+    URGENT: 'urgent',
+};
+
+
